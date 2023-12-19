9320430d1414b2601649c8be9dea41b253ccdee8
Jeribee <jerikathehologram@gmail.com>
2023-12-19 01:55:27 -0500

    New Class added. Restructured hw 1/2 code.

---
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class Lesson3
+    {
+        public static void Demo()
+        {
+            PromptString(message: "Name: ", ConsoleColor.Red);
+            string name = Console.ReadLine();
+
+            PromptString(message: "Age: ", ConsoleColor.Red);
+            int age = PromptInt();
+
+            PromptString(message: "Weight (kg): ", ConsoleColor.Red);
+            double weight = PromptFloat();
+
+            PromptString(message: "Height (cm): ", ConsoleColor.Red);
+            double height = PromptFloat();
+            double meterHeight = height / 100;
+
+            double bmi = CalculateBmi(weight, meterHeight);
+
+            PromptString(message: "\n" + name + " is " + age + " years old. Their weight is " + weight + " kg and their height is " + height + " cm. BMI is " + Math.Round(bmi, 2) + "\n", ConsoleColor.DarkGreen);
+        }
+        public static int PromptInt()
+        {
+            int result = Convert.ToInt32(Console.ReadLine());
+            return result;
+        }
+        public static void PromptString(string message, ConsoleColor newColor)
+        {
+            var oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = newColor;
+            Console.Write(message);
+            Console.ForegroundColor = oldColor;
+        }
+        static double PromptFloat()
+        {
+            double result = double.Parse(Console.ReadLine());
+            return result;
+        }
+        public static double CalculateBmi(double weight,  double meterHeight)
+        {
+            double bmi = weight / (meterHeight * meterHeight);
+            return bmi;
+        }
+    }
+}
